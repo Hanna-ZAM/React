f3fd4fdf45c44b13ee3224412bc07939ea535a32
Hanna Zalatarevich <a.zoloto@mail.ru>
2023-03-23 00:05:28 +0300

    fix: correct lint mistake

--- hanna-app/src/App.test.tsx
+++ hanna-app/src/App.test.tsx
@@ -1,16 +1,15 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-import "@testing-library/jest-dom/extend-expect";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import '@testing-library/jest-dom/extend-expect';
 
-
-describe("App", () => {
-  it("renders App component", () => {
+describe('App', () => {
+  it('renders App component', () => {
     render(<App />);
     expect(screen.getByText(/Bridge/i)).toBeInTheDocument();
     expect(screen.getByPlaceholderText('enter word for search')).toBeInTheDocument();
     expect(screen.getByAltText('search-img')).toBeInTheDocument();
-    expect(screen.getByRole('heading', {level: 2})).toBeInTheDocument();
+    expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
     expect(screen.getByDisplayValue('')).toBeInTheDocument();
   });
 });
--- hanna-app/src/components/search.test.tsx
+++ hanna-app/src/components/search.test.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import Search from "./search";
-import "@testing-library/jest-dom/extend-expect";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Search from './search';
+import '@testing-library/jest-dom/extend-expect';
 
-
-describe("search", () => {
-  it("renders App component", () => {
+describe('search', () => {
+  it('renders App component', () => {
     render(<Search />);
     expect(screen.getByPlaceholderText('enter word for search')).toBeInTheDocument();
     expect(screen.getByAltText('search-img')).toBeInTheDocument();
